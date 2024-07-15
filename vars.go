d9e5ff7c995461e51427a89ec3a617a0ff389c8e
libin <libin@zmexing.com>
2024-07-15 10:24:04 +0800

    feat: Unified definition of error

--- vars.go
+++ vars.go
@@ -1,1 +1,14 @@
 package go_redislock
+
+import "errors"
+
+var (
+	// ErrScriptException Lua脚本执行异常
+	ErrScriptException = errors.New("script execution exception")
+	// ErrScriptFailed Lua脚本执行失败
+	ErrScriptFailed = errors.New("script execution failed")
+	// ErrSpinLockTimeout 自旋锁加锁超时
+	ErrSpinLockTimeout = errors.New("spin lock timeout")
+	// ErrSpinLockDone 自旋锁加锁超时
+	ErrSpinLockDone = errors.New("spin lock context done")
+)
