eb3de258d134e80a4b81cb45d99ced0f429c8c12
libin <58850169@qq.com>
2024-07-30 16:34:04 +0800

    Feature/unit test And error Optimize Err code (#25)

--- lock_redis.go
+++ lock_redis.go
@@ -3,7 +3,6 @@ package go_redislock
 import (
 	"context"
 	_ "embed"
-	"fmt"
 	"log"
 	"time"
 )
@@ -25,11 +24,11 @@ func (lock *RedisLock) Lock() error {
 	result, err := lock.redis.Eval(lock.Context, lockScript, []string{lock.key}, lock.token, lock.lockTimeout.Seconds()).Result()
 
 	if err != nil {
-		return fmt.Errorf("%v; %v", ErrScriptException, err)
+		return ErrException
 	}
 
 	if result != "OK" {
-		return ErrScriptFailed
+		return ErrLockFailed
 	}
 
 	if lock.isAutoRenew {
@@ -53,11 +52,11 @@ func (lock *RedisLock) UnLock() error {
 	result, err := lock.redis.Eval(lock.Context, unLockScript, []string{lock.key}, lock.token).Result()
 
 	if err != nil {
-		return fmt.Errorf("%v; %v", ErrScriptException, err)
+		return ErrException
 	}
 
 	if result != "OK" {
-		return ErrScriptFailed
+		return ErrUnLockFailed
 	}
 
 	return nil
@@ -72,15 +71,14 @@ func (lock *RedisLock) SpinLock(timeout time.Duration) error {
 		}
 
 		// 加锁成功直接返回
-		err := lock.Lock()
-		if err == nil {
+		if err := lock.Lock(); err == nil {
 			return nil
 		}
 
 		// 如果加锁失败，则休眠一段时间再尝试
 		select {
 		case <-lock.Context.Done():
-			return fmt.Errorf("%v; %v", ErrSpinLockDone, lock.Context.Err()) // 处理取消操作
+			return ErrSpinLockDone // 处理取消操作
 		case <-time.After(100 * time.Millisecond):
 			// 继续尝试下一轮加锁
 		}
@@ -95,11 +93,11 @@ func (lock *RedisLock) Renew() error {
 	res, err := lock.redis.Eval(lock.Context, renewScript, []string{lock.key}, lock.token, lock.lockTimeout.Seconds()).Result()
 
 	if err != nil {
-		return fmt.Errorf("%v; %v", ErrScriptException, err)
+		return ErrException
 	}
 
 	if res != "OK" {
-		return ErrScriptFailed
+		return ErrLockRenewFailed
 	}
 
 	return nil
@@ -117,7 +115,7 @@ func (lock *RedisLock) autoRenew() {
 		case <-ticker.C:
 			err := lock.Renew()
 			if err != nil {
-				log.Println("autoRenew failed:", err)
+				log.Printf("Error: autoRenew failed, %v", err)
 				return
 			}
 		}
