c4361d05f46643c924cc063a21cca7658479d18c
libin <libin@zmexing.com>
2025-07-24 17:38:06 +0800

    feat

--- adapter_test.go
+++ adapter_test.go
@@ -2,7 +2,11 @@ package go_redislock
 
 import (
 	"context"
-	"github.com/redis/go-redis/v9"
+	mockV7 "github.com/go-redis/redismock/v7"
+	mockV8 "github.com/go-redis/redismock/v8"
+	mockV9 "github.com/go-redis/redismock/v9"
+	v9 "github.com/redis/go-redis/v9"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
 
@@ -11,10 +15,10 @@ import (
 // ----------------------------------------------------------------------------------------------
 
 type RedisMockAdapter struct {
-	client *redis.Client
+	client *v9.Client
 }
 
-func NewRedisMockAdapter(client *redis.Client) RedisInter {
+func NewRedisMockAdapter(client *v9.Client) RedisInter {
 	return &RedisMockAdapter{client: client}
 }
 
@@ -24,7 +28,7 @@ func (r *RedisMockAdapter) Eval(ctx context.Context, script string, keys []strin
 }
 
 type RedisMockCmdWrapper struct {
-	cmd *redis.Cmd
+	cmd *v9.Cmd
 }
 
 func (w *RedisMockCmdWrapper) Result() (interface{}, error) {
@@ -40,15 +44,144 @@ func (w *RedisMockCmdWrapper) Int64() (int64, error) {
 // ----------------------------------------------------------------------------------------------
 
 func TestRedisV9Adapter(t *testing.T) {
+	tests := []struct {
+		name       string
+		mock       func(t *testing.T, ctx context.Context) RedisInter
+		inputKey   string
+		inputToken string
+		wantInt64  int64
+		wantErr    error
+	}{
+		{
+			name: "Redis Adapter",
+			mock: func(t *testing.T, ctx context.Context) RedisInter {
+				db, mock := mockV9.NewClientMock()
+				mock.ExpectEval(reentrantLockScript, []string{"key"}, "token", lockTime.Milliseconds()).
+					SetVal(int64(1))
+				return NewRedisV9Adapter(db)
+			},
+			inputKey:   "key",
+			inputToken: "token",
+			wantInt64:  1,
+			wantErr:    nil,
+		},
+	}
 
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctx := context.Background()
+			rdbAdapter := tt.mock(t, ctx)
+
+			cmd := rdbAdapter.Eval(ctx, reentrantLockScript, []string{tt.inputKey}, tt.inputToken, lockTime.Milliseconds())
+			_, err := cmd.Result()
+			if tt.wantErr != nil {
+				require.NoError(t, err)
+			}
+
+			i, err := cmd.Int64()
+			if tt.wantErr != nil {
+				require.NoError(t, err)
+			}
+
+			if i != tt.wantInt64 {
+				t.Errorf("Expected %d, got %d", tt.wantInt64, i)
+			}
+		})
+	}
 }
 
 func TestRedisV8Adapter(t *testing.T) {
+	tests := []struct {
+		name       string
+		mock       func(t *testing.T, ctx context.Context) RedisInter
+		inputKey   string
+		inputToken string
+		wantInt64  int64
+		wantErr    error
+	}{
+		{
+			name: "Redis Adapter",
+			mock: func(t *testing.T, ctx context.Context) RedisInter {
+				db, mock := mockV8.NewClientMock()
+				mock.ExpectEval(reentrantLockScript, []string{"key"}, "token", lockTime.Milliseconds()).
+					SetVal(int64(1))
+				return NewRedisV8Adapter(db)
+			},
+			inputKey:   "key",
+			inputToken: "token",
+			wantInt64:  1,
+			wantErr:    nil,
+		},
+	}
 
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctx := context.Background()
+			rdbAdapter := tt.mock(t, ctx)
+
+			cmd := rdbAdapter.Eval(ctx, reentrantLockScript, []string{tt.inputKey}, tt.inputToken, lockTime.Milliseconds())
+			_, err := cmd.Result()
+			if tt.wantErr != nil {
+				require.NoError(t, err)
+			}
+
+			i, err := cmd.Int64()
+			if tt.wantErr != nil {
+				require.NoError(t, err)
+			}
+
+			if i != tt.wantInt64 {
+				t.Errorf("Expected %d, got %d", tt.wantInt64, i)
+			}
+		})
+	}
 }
 
 func TestRedisV7Adapter(t *testing.T) {
+	tests := []struct {
+		name       string
+		mock       func(t *testing.T, ctx context.Context) RedisInter
+		inputKey   string
+		inputToken string
+		wantInt64  int64
+		wantErr    error
+	}{
+		{
+			name: "Redis Adapter",
+			mock: func(t *testing.T, ctx context.Context) RedisInter {
+				db, mock := mockV7.NewClientMock()
+				mock.ExpectEval(reentrantLockScript, []string{"key"}, "token", lockTime.Milliseconds()).
+					SetVal(int64(1))
+				return NewRedisV7Adapter(db)
+			},
+			inputKey:   "key",
+			inputToken: "token",
+			wantInt64:  1,
+			wantErr:    nil,
+		},
+	}
 
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctx := context.Background()
+			rdbAdapter := tt.mock(t, ctx)
+
+			cmd := rdbAdapter.Eval(ctx, reentrantLockScript, []string{tt.inputKey}, tt.inputToken, lockTime.Milliseconds())
+			_, err := cmd.Result()
+			if tt.wantErr != nil {
+				require.NoError(t, err)
+			}
+
+			i, err := cmd.Int64()
+			if tt.wantErr != nil {
+				require.NoError(t, err)
+			}
+
+			if i != tt.wantInt64 {
+				t.Errorf("Expected %d, got %d", tt.wantInt64, i)
+			}
+		})
+	}
 }
 
 func TestGoZeroRedisAdapter(t *testing.T) {
--- go.mod
+++ go.mod
@@ -7,12 +7,15 @@ retract v1.0.0
 require (
 	github.com/go-redis/redis/v7 v7.4.1
 	github.com/go-redis/redis/v8 v8.11.5
+	github.com/go-redis/redismock/v7 v7.0.5
+	github.com/go-redis/redismock/v8 v8.11.5
 	github.com/go-redis/redismock/v9 v9.2.0
 	github.com/gogf/gf/v2 v2.9.0
 	github.com/google/uuid v1.6.0
 	github.com/redis/go-redis/v9 v9.11.0
 	github.com/stretchr/testify v1.10.0
 	github.com/zeromicro/go-zero v1.8.5
+	go.uber.org/mock v0.5.2
 )
 
 require (
