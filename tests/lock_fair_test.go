450e7258899a855302ba404f939a4c199b16efe1
libin <libin@zmexing.com>
2025-10-23 19:21:23 +0800

    单元测试

--- tests/lock_fair_test.go
+++ tests/lock_fair_test.go
@@ -1,1 +1,50 @@
 package tests
+
+import (
+	"context"
+	"errors"
+	"testing"
+	"time"
+
+	redislock "github.com/jefferyjob/go-redislock"
+)
+
+func Test_FairLock(t *testing.T) {
+	adapter := getRedisClient()
+
+	tests := []struct {
+		name       string
+		inputKey   string
+		inputReqId string
+		sleepTime  time.Duration // 模拟业务执行时间
+		wantErr    error
+	}{
+		{
+			name:       "公平锁-加锁成功",
+			inputKey:   "test_key",
+			inputReqId: "test_req",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctx := context.Background()
+
+			lock := redislock.New(adapter, tt.inputKey,
+				redislock.WithAutoRenew(),
+				redislock.WithRequestTimeout(lockTime), // 设置公平锁请求超时时间
+			)
+
+			err := lock.FairLock(ctx, tt.inputReqId)
+			if !errors.Is(err, tt.wantErr) {
+				t.Errorf("Expected error = %v, wantErr %v", err, tt.wantErr)
+			}
+			defer lock.FairUnLock(ctx, tt.inputReqId)
+
+			if tt.sleepTime != time.Duration(0) {
+				// 模拟业务执行时间
+				time.Sleep(tt.sleepTime)
+			}
+		})
+	}
+}
--- tests/rdb_test.go
+++ tests/rdb_test.go
@@ -2,18 +2,26 @@ package tests
 
 import (
 	"fmt"
+	"time"
 
 	redislock "github.com/jefferyjob/go-redislock"
 	adapter "github.com/jefferyjob/go-redislock/adapter/go-redis/v9"
 	"github.com/redis/go-redis/v9"
 )
 
+const (
+	// 默认锁超时时间
+	lockTime = 5 * time.Second
+	// 默认请求超时时间
+	requestTimeout = lockTime
+)
+
 var (
-	addr         = "127.0.0.1"
-	port         = "63790"
-	luaSetScript = `return redis.call("SET", KEYS[1], ARGV[1])`
-	luaGetScript = `return redis.call("GET", KEYS[1])`
-	luaDelScript = `return redis.call("DEL", KEYS[1])`
+	addr = "127.0.0.1"
+	port = "63790"
+	// luaSetScript = `return redis.call("SET", KEYS[1], ARGV[1])`
+	// luaGetScript = `return redis.call("GET", KEYS[1])`
+	// luaDelScript = `return redis.call("DEL", KEYS[1])`
 )
 
 // Redis服务器测试
