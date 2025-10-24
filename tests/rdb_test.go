fe515bbf0102e9fc01c8ae16a55edcd4b898cd0d
libin <libin@zmexing.com>
2025-10-24 16:31:07 +0800

    单元测试

--- tests/rdb_test.go
+++ tests/rdb_test.go
@@ -2,6 +2,7 @@ package tests
 
 import (
 	"fmt"
+	"sync"
 	"time"
 
 	redislock "github.com/jefferyjob/go-redislock"
@@ -22,15 +23,26 @@ var (
 	// luaSetScript = `return redis.call("SET", KEYS[1], ARGV[1])`
 	// luaGetScript = `return redis.call("GET", KEYS[1])`
 	// luaDelScript = `return redis.call("DEL", KEYS[1])`
+
+	once       sync.Once
+	redisInter redislock.RedisInter
 )
 
-// Redis服务器测试
-// 下面的代码将借助 redis 服务器进行测试，可以更加方便的测试服务中的问题
-// 你可以实用下面的命令启动一个redis容器进行测试
-// docker run -d -p 63790:6379 --name go_redis_lock redis
+// Redis 服务器集成测试
+//
+// 本测试依赖实际的 Redis 服务，用于验证分布式锁在真实环境下的行为。
+// 你可以通过以下命令快速启动一个本地 Redis 容器：
+//
+//	docker run -d -p 63790:6379 --name go_redis_lock redis
+//
+// 运行后，测试代码将自动连接到该容器的 Redis 实例，
+// 以便更方便地调试和定位服务中的潜在问题。
 func getRedisClient() redislock.RedisInter {
-	rdb := redis.NewClient(&redis.Options{
-		Addr: fmt.Sprintf("%s:%s", addr, port),
+	once.Do(func() {
+		rdb := redis.NewClient(&redis.Options{
+			Addr: fmt.Sprintf("%s:%s", addr, port),
+		})
+		redisInter = adapter.New(rdb)
 	})
-	return adapter.New(rdb)
+	return redisInter
 }
